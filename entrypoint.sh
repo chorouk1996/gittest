da862d9065ba766f6985707454d191922c323134
Chorouk Amahri <chorouk.amahri@ibm.com>
2020-09-24 23:17:30 +0100

    commit commands.sh and generating genesis block + anchor peers

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-cp -R /sources/config /fabric/config
+cp -R /sources/config/* /fabric/
+ls /fabric/
 cp -R /sources/base /fabric/base
-
-ls /fabric/config
-ls /fabric/base
-cat /fabric/base/fabric-ca-server-config-bank2-banksco.yaml
 exit 0
