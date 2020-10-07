05049289a123d1b6d2a2454a7e0746826ce13d4f
Chorouk Amahri <chorouk.amahri@ibm.com>
2020-10-07 17:29:08 +0100

    add script

--- config/channelclibank2.sh
+++ config/channelclibank2.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 peer channel fetch newest $CHANNEL_NAME.block -o orderer:7050 -c $CHANNEL_NAME --tls --cafile $ORDERER_TLS_CA
 export CORE_PEER_TLS_ROOTCERT_FILE=$PEER2_BANK2_TLS_CA
 export CORE_PEER_ADDRESS=$PEER2_BANK2_PRADDR
