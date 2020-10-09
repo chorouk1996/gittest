aba868830900c5be28343c5881f6dc616ec3eee5
Adrien Marchal <adrien.marchal-isc_france@fr.ibm.com>
2020-10-09 08:51:27 +0000

    Delete entrypoint.sh

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cp -R /sources/config/* /fabric-secret/
-cp -R /sources/base /fabric-secret/base
-#exit 0 #to be decommented in final version
-
-#Note : needed instruction for test! otherwise the pod immediately goes in "Completed" status.
-#		If the Pod is completed, there's nothing running you can exec into.
-sleep 24h #instruction for test purpose
