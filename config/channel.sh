da407f43760142fc610010259df440c4988b5849
Chorouk Amahri <chorouk.amahri@ibm.com>
2020-10-22 11:28:31 +0100

    change cc-pkg-config

--- config/channel.sh
+++ config/channel.sh
@@ -7,4 +7,4 @@ peer channel join -b bankscochannel.block
 export CORE_PEER_TLS_ROOTCERT_FILE=$PEER1_BANK1_TLS_CA
 export CORE_PEER_ADDRESS=$PEER1_BANK1_PRADDR
 peer channel join -b bankscochannel.block
-peer channel update -o orderer:7050 -c $CHANNEL_NAME -f /fabric/channel-artifacts/Bank1MSPanchors.tx --tls --cafile $ORDERER_TLS_CA
+#peer channel update -o orderer:7050 -c $CHANNEL_NAME -f /fabric/channel-artifacts/Bank1MSPanchors.tx --tls --cafile $ORDERER_TLS_CA
