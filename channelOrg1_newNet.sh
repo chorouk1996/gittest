9bbb3619a2de9cea2d2bfea27e4247b6699489c6
Adrien Marchal <adrien.marchal-isc_france@fr.ibm.com>
2020-11-20 16:18:56 +0000

    Upload New File

---
+++ channelOrg1_newNet.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+rm  /etc/hyperledger/fabric/core.yaml 
+cp  /fabric/core.yaml /etc/hyperledger/fabric/
+peer channel create -o ordererTF1.banksco.com:7050  -c $CHANNEL_NAME -f /fabric/channel-artifacts/channel.tx --tls true --cafile $ORDERER_TLS_CA
+export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_BANK1_TLS_CA
+export CORE_PEER_ADDRESS=$PEER0_BANK1_PRADDR
+peer channel join -b bankscochannel.block
+peer channel update -o ordererTF1.banksco.com:7050 -c $CHANNEL_NAME -f /fabric/channel-artifacts/Bank1MSPanchors.tx --tls --cafile $ORDERER_TLS_CA
