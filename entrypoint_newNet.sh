f6f08f50dfac78246f09ecae5e4ebee8244557a4
Adrien Marchal <adrien.marchal-isc_france@fr.ibm.com>
2020-11-20 16:32:17 +0000

    Upload New File

---
+++ entrypoint_newNet.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cp -R /sources/base /fabric/base
+cp -R /sources/config/* /fabric/
+#cp -R /sources/buildpack /fabric/buildpack
+#chmod -R 777 /fabric/buildpack/bin
+#cp -R /sources/cc-pkg-config /fabric/cc-pkg-config
+exit 0
